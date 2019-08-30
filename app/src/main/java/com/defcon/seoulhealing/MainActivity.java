08c2c05dcc0d3ebebbfd2bb4728b4f57c4d78cc9
Caret <wcle3456@gmail.com>
2019-08-30 14:41:13 +0900

    Add Splash code

--- app/src/main/java/com/defcon/seoulhealing/MainActivity.java
+++ app/src/main/java/com/defcon/seoulhealing/MainActivity.java
@@ -2,6 +2,7 @@ package com.defcon.seoulhealing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //스플래시 화면 실행
+        startActivity(new Intent(getBaseContext(), SplashActivity.class));
     }
 }
