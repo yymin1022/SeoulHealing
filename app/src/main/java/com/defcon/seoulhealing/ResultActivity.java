b113a3bd204030dcc3b35b41503d1530fbf8450f
yymin1022 <yymin1022@gmail.com>
2019-08-30 16:24:16 +0900

    Add Result Activity

---
+++ app/src/main/java/com/defcon/seoulhealing/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.defcon.seoulhealing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
