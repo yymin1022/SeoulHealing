cbccfa1d020bfa37ad649daf235f2369410acdd8
yymin1022 <yymin1022@gmail.com>
2019-09-04 17:05:17 +0900

    Update List Adapter

--- app/src/main/java/com/defcon/seoulhealing/HealingListAdapter.java
+++ app/src/main/java/com/defcon/seoulhealing/HealingListAdapter.java
@@ -1,4 +1,65 @@
 package com.defcon.seoulhealing;
 
-public class HealingListAdapter {
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class HealingListAdapter extends BaseAdapter{
+    private ArrayList<HealingListItem> listViewItemList = new ArrayList<HealingListItem>();
+
+    public HealingListAdapter(){
+    }
+
+    @Override
+    public int getCount(){
+        return listViewItemList.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        final Context context = parent.getContext();
+
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_healing_item, parent, false);
+        }
+
+        ImageView iconImageView = convertView.findViewById(R.id.list_item_img);
+        TextView titleTextView = convertView.findViewById(R.id.list_item_title);
+        TextView descTextView = convertView.findViewById(R.id.list_item_address);
+
+        HealingListItem listViewItem = listViewItemList.get(position);
+        iconImageView.setImageDrawable(listViewItem.getImageDrawable());
+        titleTextView.setText(listViewItem.getTitleStr());
+        descTextView.setText(listViewItem.getAddressStr());
+
+        return convertView;
+    }
+
+    @Override
+    public long getItemId(int position){
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position){
+        return listViewItemList.get(position);
+    }
+
+    public void addItem(Drawable icon, String title, String desc){
+        HealingListItem item = new HealingListItem();
+
+        item.setImageDrawable(icon);
+        item.setTitleStr(title);
+        item.setAddressStr(desc);
+
+        listViewItemList.add(item);
+    }
 }
--- app/src/main/java/com/defcon/seoulhealing/HealingListItem.java
+++ app/src/main/java/com/defcon/seoulhealing/HealingListItem.java
@@ -1,4 +1,29 @@
 package com.defcon.seoulhealing;
 
+import android.graphics.drawable.Drawable;
+
 public class HealingListItem {
+    private Drawable imageDrawable ;
+    private String titleStr ;
+    private String addressStr ;
+
+    public void setImageDrawable(Drawable icon) {
+        imageDrawable = icon ;
+    }
+    public void setTitleStr(String title) {
+        titleStr = title ;
+    }
+    public void setAddressStr(String desc) {
+        addressStr = desc ;
+    }
+
+    public Drawable getImageDrawable() {
+        return this.imageDrawable ;
+    }
+    public String getTitleStr() {
+        return this.titleStr ;
+    }
+    public String getAddressStr() {
+        return this.addressStr ;
+    }
 }
