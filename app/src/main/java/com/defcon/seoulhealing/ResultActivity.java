ada570ef1dd44f643d6ade7dc3f0094e7493c045
yymin1022 <yymin1022@gmail.com>
2019-09-08 17:28:12 +0900

    Update ResultActivity : Fix Get Json Code

--- app/src/main/java/com/defcon/seoulhealing/ResultActivity.java
+++ app/src/main/java/com/defcon/seoulhealing/ResultActivity.java
@@ -22,11 +22,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity{
     String API_KEY = "e284ba69d2b34c208f15160fdb8a8e8c";
     String API_THEME = "";
     String API_URL_DEFAULT = "https://map.seoul.go.kr/smgis/apps/theme.do?cmd=getContentsList&page_no=1&page_size=9&key=%s&coord_x=126.9752884&coord_y=37.5649732&distance=3000&search_type=0&search_name=&theme_id=%s&subcate_id=100061,1";
@@ -37,7 +38,7 @@ public class ResultActivity extends AppCompatActivity {
     ListView resultList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         resultList = findViewById(R.id.result_list_healing);
@@ -51,36 +52,38 @@ public class ResultActivity extends AppCompatActivity {
         new getJSON().execute();
     }
 
-    private class getJSON extends AsyncTask<String, Void, String> {
+    private class getJSON extends AsyncTask<String, Void, String>{
         ProgressDialog progressDialog = new ProgressDialog(ResultActivity.this);
 
         @Override
-        protected void onPreExecute() {
+        protected void onPreExecute(){
             super.onPreExecute();
             progressDialog.setTitle("Loading...");
             progressDialog.show();
         }
 
         @Override
-        protected String doInBackground(String... strs) {
-            try {
+        protected String doInBackground(String... strs){
+            try{
                 String jsonPage = getStringFromUrl(API_URL);
                 JSONObject json = new JSONObject(jsonPage);
                 JSONArray jArr = json.getJSONArray("body");
 
-                itemData = new ArrayList<HealingListItem>();
+                itemData = new ArrayList<>();
 
-                for (int i=0; i<jArr.length(); i++){
+                for(int i=0; i<jArr.length(); i++){
                     json = jArr.getJSONObject(i);
                     itemData.add(new HealingListItem(null, json.getString("COT_VALUE_01"), json.getString("THM_THEME_NAME"), json.getString("COT_ADDR_FULL_NEW")));
                 }
+
                 listAdapter = new HealingListAdapter(ResultActivity.this, itemData);
-            } catch (Exception e) {
+            }catch(Exception e){
+                Log.e("Error", e.toString());
             }
             return "OK";
         }
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(String result){
             resultList.setAdapter(listAdapter);
             progressDialog.cancel();
         }
@@ -91,30 +94,28 @@ public class ResultActivity extends AppCompatActivity {
         HttpURLConnection urlConnection = null;
 
         StringBuffer page=new StringBuffer();
-        try {
+        try{
             URL url= new URL(pUrl);
             urlConnection = (HttpURLConnection) url.openConnection();
             InputStream contentStream = urlConnection.getInputStream();
 
-            bufreader = new BufferedReader(new InputStreamReader(contentStream,"UTF-8"));
-            String line = null;
+            bufreader = new BufferedReader(new InputStreamReader(contentStream, StandardCharsets.UTF_8));
+            String line;
 
             while((line = bufreader.readLine())!=null){
                 page.append(line);
             }
 
-        } catch (IOException e) {
+        }catch(IOException e){
             e.printStackTrace();
         }finally{
-            try {
+            try{
                 bufreader.close();
                 urlConnection.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
+            }catch(Exception e){
+                Log.e("Error", e.toString());
             }
-
         }
-
         return page.toString();
     }
 }
