e1bca111c097760498c248777f04e11b46c6c1df
yymin1022 <yymin1022@gmail.com>
2019-09-08 16:02:52 +0900

    Update ResultActivity : Get Theme Code from Selection

--- app/src/main/java/com/defcon/seoulhealing/MainActivity.java
+++ app/src/main/java/com/defcon/seoulhealing/MainActivity.java
@@ -25,20 +25,29 @@ public class MainActivity extends AppCompatActivity {
         Button.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent themeIntent = new Intent(MainActivity.this, ResultActivity.class);
                 switch(view.getId()){
                     case R.id.main_btn_theme1:
                         Toast.makeText(MainActivity.this, "Theme 1", Toast.LENGTH_SHORT).show();
+                        themeIntent.putExtra("THEME", "100200,100214,100162,100167");
                         break;
                     case R.id.main_btn_theme2:
                         Toast.makeText(MainActivity.this, "Theme 2", Toast.LENGTH_SHORT).show();
+                        themeIntent.putExtra("THEME", "100736,100362,100235,100273");
                         break;
                     case R.id.main_btn_theme3:
                         Toast.makeText(MainActivity.this, "Theme 3", Toast.LENGTH_SHORT).show();
+                        themeIntent.putExtra("THEME", "100123,100211,100304,100569,100715,11107096,100891,11101181");
                         break;
                     case R.id.main_btn_theme4:
                         Toast.makeText(MainActivity.this, "Theme 4", Toast.LENGTH_SHORT).show();
+                        themeIntent.putExtra("THEME", "100361,100769,100325");
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                         break;
                 }
+                startActivity(themeIntent);
             }
         };
 
--- app/src/main/java/com/defcon/seoulhealing/ResultActivity.java
+++ app/src/main/java/com/defcon/seoulhealing/ResultActivity.java
@@ -2,17 +2,24 @@ package com.defcon.seoulhealing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Locale;
 
 public class ResultActivity extends AppCompatActivity {
-    String API_KEY = "";
-    String API_URL = String.format(Locale.getDefault(), "https://map.seoul.go.kr/smgis/apps/theme.do?cmd=getContentsList&page_no=1&page_size=9&key=%s&coord_x=126.9752884&coord_y=37.5649732&distance=3000&search_type=0&search_name=&theme_id=100123&subcate_id=100061,1", API_KEY);
+    String API_KEY = "e284ba69d2b34c208f15160fdb8a8e8c";
+    String API_THEME = "";
+    String API_URL = String.format(Locale.getDefault(), "https://map.seoul.go.kr/smgis/apps/theme.do?cmd=getContentsList&page_no=1&page_size=9&key=%s&coord_x=126.9752884&coord_y=37.5649732&distance=3000&search_type=0&search_name=&theme_id=%s&subcate_id=100061,1", API_KEY, API_THEME);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Intent themeIntent = getIntent();
+        API_THEME = themeIntent.getStringExtra("THEME");
+
+        Log.d("THEME", API_THEME);
     }
 }
