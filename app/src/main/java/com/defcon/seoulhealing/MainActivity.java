22f3ed36c2f3014d265278b11ae5b81c8bd6a480
Caret <wcle3456@gmail.com>
2019-09-20 12:10:19 +0900

    Update MainActivity : Add Animation

--- app/src/main/java/com/defcon/seoulhealing/MainActivity.java
+++ app/src/main/java/com/defcon/seoulhealing/MainActivity.java
@@ -1,11 +1,14 @@
 package com.defcon.seoulhealing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 //        if(prefs.getBoolean("isFirst", true)){
             startActivity(new Intent(this, WelcomeActivity.class));
 //        }
+        startActivityAnimation();
     }
 
     @Override
@@ -167,10 +171,15 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        ImageButton btnActivity = findViewById(R.id.main_btn_activity);
-        ImageButton btnChild = findViewById(R.id.main_btn_child);
-        ImageButton btnRelax = findViewById(R.id.main_btn_relax);
-        ImageButton btnTravel = findViewById(R.id.main_btn_travel);
+        final ImageButton btnActivity = findViewById(R.id.main_btn_activity);
+        final ImageButton btnChild = findViewById(R.id.main_btn_child);
+        final ImageButton btnRelax = findViewById(R.id.main_btn_relax);
+        final ImageButton btnTravel = findViewById(R.id.main_btn_travel);
+
+        btnActivity.setVisibility(View.VISIBLE);
+        btnChild.setVisibility(View.VISIBLE);
+        btnRelax.setVisibility(View.VISIBLE);
+        btnTravel.setVisibility(View.VISIBLE);
 
         Button.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -180,21 +189,24 @@ public class MainActivity extends AppCompatActivity {
                 switch(view.getId()){
                     case R.id.main_btn_activity:
                         themeIntent.putExtra("THEME", "ACTIVITY");
+                        setThemeButtonAnimation(btnActivity, R.anim.scale_btn_activity, btnChild, btnRelax, btnTravel, themeIntent);
                         break;
                     case R.id.main_btn_child:
                         themeIntent.putExtra("THEME", "CHILD");
+                        setThemeButtonAnimation(btnChild, R.anim.scale_btn_child, btnActivity, btnRelax, btnTravel, themeIntent);
                         break;
                     case R.id.main_btn_relax:
                         themeIntent.putExtra("THEME", "RELAX");
+                        setThemeButtonAnimation(btnRelax, R.anim.scale_btn_relax, btnActivity, btnChild, btnTravel, themeIntent);
                         break;
                     case R.id.main_btn_travel:
                         themeIntent.putExtra("THEME", "TRAVEL");
+                        setThemeButtonAnimation(btnTravel, R.anim.scale_btn_travel, btnActivity, btnChild, btnRelax, themeIntent);
                         break;
                     default:
                         Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                         break;
                 }
-                startActivity(themeIntent);
             }
         };
 
@@ -202,5 +214,72 @@ public class MainActivity extends AppCompatActivity {
         btnChild.setOnClickListener(onClickListener);
         btnRelax.setOnClickListener(onClickListener);
         btnTravel.setOnClickListener(onClickListener);
+    }
+
+    private void startActivityAnimation() {
+        ImageView locationMap = findViewById(R.id.main_image_location_map);
+        CardView cardLocation = findViewById(R.id.main_card_location);
+        ImageButton btnActivity = findViewById(R.id.main_btn_activity);
+        ImageButton btnChild = findViewById(R.id.main_btn_child);
+        ImageButton btnRelax = findViewById(R.id.main_btn_relax);
+        ImageButton btnTravel = findViewById(R.id.main_btn_travel);
+
+        Animation locationMapAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_location_map);
+        Animation cardLocationAnim = AnimationUtils.loadAnimation(this, R.anim.scale_card_location);
+        Animation btnActivityAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_btn_activity);
+        Animation btnChildAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_btn_child);
+        Animation btnRelaxAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_btn_relax);
+        Animation btnTravelAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in_btn_travel);
+
+        locationMap.startAnimation(locationMapAnim);
+        cardLocation.startAnimation(cardLocationAnim);
+        btnActivity.startAnimation(btnActivityAnim);
+        btnChild.startAnimation(btnChildAnim);
+        btnRelax.startAnimation(btnRelaxAnim);
+        btnTravel.startAnimation(btnTravelAnim);
+    }
+
+    private void setThemeButtonAnimation(final ImageButton selectButton, int animEffect, final ImageButton otherButton1, final ImageButton otherButton2, final ImageButton otherButton3, final Intent intent) {
+
+        Animation scale = AnimationUtils.loadAnimation(this, animEffect);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+
+        selectButton.startAnimation(scale);
+        otherButton1.startAnimation(fadeOut);
+        otherButton2.startAnimation(fadeOut);
+        otherButton3.startAnimation(fadeOut);
+
+        selectButton.setClickable(false);
+        otherButton1.setClickable(false);
+        otherButton2.setClickable(false);
+        otherButton3.setClickable(false);
+
+        scale.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                selectButton.setVisibility(View.INVISIBLE);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                otherButton1.setVisibility(View.INVISIBLE);
+                otherButton2.setVisibility(View.INVISIBLE);
+                otherButton3.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
     }
 }
